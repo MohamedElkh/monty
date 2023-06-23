77c7c0afa59d38df18f3dcc045e71b3ba4ddc087
Foday Malike Kabah <38090300+231AG@users.noreply.github.com>
2023-06-23 09:33:38 +0000

    Final bug fixed

--- func_monty4.c
+++ func_monty4.c
@@ -7,3 +7,102 @@
  * Return: no return
  */
 
+void addqueue(stack_t **main, int num)
+{
+	stack_t *newnode;
+	stack_t *ax;
+
+	ax = *main;
+
+	newnode = malloc(sizeof(stack_t));
+
+	if (newnode == NULL)
+	{
+		printf("Error\n");
+	}
+
+	newnode->n = num;
+	newnode->next = NULL;
+
+	if (ax)
+	{
+		while (ax->next)
+		{
+			ax = ax->next;
+		}
+	}
+
+	if (!ax)
+	{
+		*main = newnode;
+
+		newnode->prev = NULL;
+	}
+	else
+	{
+		ax->next = newnode;
+		newnode->prev = ax;
+	}
+}
+
+/**
+ * func_queue - funct to funct to prints the top
+ * @main: the stack head
+ * @count: the line_number
+ * Return: no return
+ */
+
+void func_queue(stack_t **main, unsigned int count)
+{
+	(void)main;
+	(void)count;
+	bovalue.lifev = 1;
+}
+
+/**
+ * func_stack - func to prints the top
+ * @main: the stack head
+ * @count: the line_number
+ * Return: no return
+ */
+
+void func_stack(stack_t **main, unsigned int count)
+{
+	(void)main;
+	(void)count;
+	bovalue.lifev = 0;
+}
+
+/**
+ * addnode - func to add node to the head stack
+ * @main: the head of the stack
+ * @n: the new_value
+ * Return: no return
+ */
+void addnode(stack_t **main, int num)
+{
+
+	stack_t *newnode;
+	stack_t *aux;
+
+	aux = *main;
+
+	newnode = malloc(sizeof(stack_t));
+
+	if (newnode == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+
+	if (aux)
+	{
+		aux->prev = newnode;
+	}
+
+	newnode->n = num;
+	newnode->next = *main;
+
+	newnode->prev = NULL;
+	*main = newnode;
+}
